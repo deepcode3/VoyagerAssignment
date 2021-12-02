983529e3ce3f484dc79f6f2b105ffb759f6e6413
Rahul NK <rahul.nk@robosoftin.com>
2021-12-02 21:27:23 +0530

    home component commit

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Logo from '../../components/logo';
+import SearchBar from '../../components/homeComponents/searchbar';
 import './home.css';
 const Home = () => {
 	return (
-		<>
-			<div className="homeBg"></div>
-		</>
+		<div className="homeBg">
+			<div className="homeLogo">
+				<Logo />
+			</div>
+			<div className="homeInfo">
+				<span className="homeInfo1">Order your food</span>
+				<br />
+				<span className="homeInfo2">from the best restaurants</span>
+			</div>
+			<SearchBar />
+		</div>
 	);
 };
 export default Home;
