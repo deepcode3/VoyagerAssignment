82d262e90a9d68f61266b2339e01c5f97d7a25f5
Rahul NK <rahul.nk@robosoftin.com>
2021-12-07 23:54:06 +0530

    filter commit

--- src/containers/restaurants/index.js
+++ src/containers/restaurants/index.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import Logo from '../../components/logo';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControl from '@material-ui/core/FormControl';
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 import SearchBar, {
 	LocationSetter,
 } from '../../components/homeComponents/searchbar';
@@ -13,6 +18,18 @@ import checkBox from '../../assets/icons/checkBox.png';
 import './restaurants.css';
 const Restaurant = ({ data, searchKey, location }) => {
 	const [TimeSortHide, setTimeSortHide] = useState(false);
+	const [MinimumOrderHide, setMinimumOrderHide] = useState(false);
+	const [deliveryTimevalue, setDeliveryTimeValue] = useState('30');
+	const [averageMealCostValue, setAveregaeMealCostValue] = useState('30');
+	const [minimumOrderValue, setMinimumOrderValue] = useState('60');
+	const theme = createTheme({
+		palette: {
+			primary: { main: '#FFA500' },
+			ßsecondary: {
+				main: '#FFA500',
+			},
+		},
+	});
 	return (
 		<>
 			<div className="header">
@@ -57,7 +74,16 @@ const Restaurant = ({ data, searchKey, location }) => {
 					<div className="restaurantBody">
 						<div className="filterBox">
 							<div className="filterHeading">Filters</div>
-							<div className="resetAll">Reset All</div>
+							<div
+								className="resetAll"
+								onClick={() => {
+									setDeliveryTimeValue('30');
+									setAveregaeMealCostValue('30');
+									setMinimumOrderValue('60');
+								}}
+							>
+								Reset All
+							</div>
 							<div className="filterUnderLine"></div>
 							<div className="timeAndAverageMealCost">
 								<div className="timeInfo">TIME & AVEREAGE MEAL COST</div>
@@ -81,7 +107,11 @@ const Restaurant = ({ data, searchKey, location }) => {
 									></img>
 								)}
 							</div>
-							{!TimeSortHide ? <div className="timeSortUnderLine"></div> : null}
+							<div
+								className={
+									!TimeSortHide ? 'timeSortUnderLine1' : 'timeSortUnderLine2'
+								}
+							></div>
 							<div
 								className={TimeSortHide ? 'timeSortInfo' : 'revTimeSortInfo'}
 							>
@@ -96,9 +126,166 @@ const Restaurant = ({ data, searchKey, location }) => {
 										<span className="statusButtonLabel">Open Now</span>
 									</div>
 								</div>
-								<div className="deliverTime"></div>
-								<div className="averageMealCost"></div>
-								<div className="minimumOrder"></div>
+								<div className="deliveryTime">
+									<div className="deliverTimeLabel">Delivery in</div>
+									<div className="deliveryTimeFilter">
+										<MuiThemeProvider theme={theme}>
+											<FormControl component="fieldset">
+												<RadioGroup
+													aria-label="minutes"
+													name="minutes1"
+													value={deliveryTimevalue}
+													onChange={(e) => setDeliveryTimeValue(e.target.value)}
+													className="timeRadioButtons"
+												>
+													<FormControlLabel
+														value="30"
+														control={<Radio color="primary" />}
+														label="30 min"
+														className={
+															deliveryTimevalue === '30'
+																? 'selected'
+																: 'disSelected'
+														}
+													/>
+													<FormControlLabel
+														value="45"
+														control={<Radio color="primary" />}
+														label="45 min"
+														className={
+															deliveryTimevalue === '45'
+																? 'selected'
+																: 'disSelected'
+														}
+													/>
+													<FormControlLabel
+														value="60"
+														control={<Radio color="primary" />}
+														label="60 min"
+														className={
+															deliveryTimevalue === '60'
+																? 'selected'
+																: 'disSelected'
+														}
+													/>
+												</RadioGroup>
+											</FormControl>
+										</MuiThemeProvider>
+									</div>
+								</div>
+								<div className="averageMealCost">
+									<div className="averageMealCostLabel">Average Meal Cost</div>
+									<div className="avereageMealCostFiltter">
+										<MuiThemeProvider theme={theme}>
+											<FormControl component="fieldset">
+												<RadioGroup
+													aria-label="AED"
+													name="AED1"
+													value={averageMealCostValue}
+													onChange={(e) =>
+														setAveregaeMealCostValue(e.target.value)
+													}
+													className="timeRadioButtons"
+												>
+													<FormControlLabel
+														value="30"
+														control={<Radio color="primary" />}
+														label="Upto 30 AED"
+														className={
+															averageMealCostValue === '30'
+																? 'selected'
+																: 'disSelected'
+														}
+													/>
+													<FormControlLabel
+														value="60"
+														control={<Radio color="primary" />}
+														label="Upto 60 AED"
+														className={
+															averageMealCostValue === '60'
+																? 'selected'
+																: 'disSelected'
+														}
+													/>
+													<FormControlLabel
+														value="100"
+														control={<Radio color="primary" />}
+														label="Upto 100 AED"
+														className={
+															averageMealCostValue === '100'
+																? 'selected'
+																: 'disSelected'
+														}
+													/>
+												</RadioGroup>
+											</FormControl>
+										</MuiThemeProvider>
+									</div>
+								</div>
+								<div className="minimumOrder">
+									<div className="minimumOrderLabel">Minimum order</div>
+									<div className="minimumOrderFilter">
+										<MuiThemeProvider theme={theme}>
+											<FormControl component="fieldset">
+												<RadioGroup
+													aria-label="minimum"
+													name="minimum"
+													value={minimumOrderValue}
+													onChange={(e) => setMinimumOrderValue(e.target.value)}
+													className="timeRadioButtons"
+												>
+													<FormControlLabel
+														value="60"
+														control={<Radio color="primary" />}
+														label="Upto 60 AED"
+														className={
+															minimumOrderValue === '60'
+																? 'selected'
+																: 'disSelected'
+														}
+													/>
+													<FormControlLabel
+														value="100"
+														control={<Radio color="primary" />}
+														label="Upto 100 AED"
+														className={
+															minimumOrderValue === '100'
+																? 'selected'
+																: 'disSelected'
+														}
+													/>
+												</RadioGroup>
+											</FormControl>
+										</MuiThemeProvider>
+									</div>
+								</div>
+							</div>
+							<div className="cuisines">
+								<div className="cuisinesLabel">CUISINES</div>
+								{!MinimumOrderHide ? (
+									<img
+										className="cuisinesShowButton"
+										src={showButton}
+										alt="timeextendhideButton"
+										onClick={() => {
+											setMinimumOrderHide(true);
+										}}
+									></img>
+								) : (
+									<img
+										className="cuisinesHideButton"
+										src={hideButton}
+										alt="timeextendhideButton"
+										onClick={() => {
+											setMinimumOrderHide(false);
+										}}
+									></img>
+								)}
+							</div>
+							{MinimumOrderHide ? <div className="cuisinesInfo"></div> : null}
+							<div className="cuisinesSortUnderLine"></div>
+							<div className="applyButtonBox">
+								<span className="applyButtonLabel">APPLY</span>
 							</div>
 						</div>
 						{data.map((res, index) => {
