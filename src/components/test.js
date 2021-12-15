64d8a73eb80e3e21a7a7b251d20a7bb34c0dc526
Rahul NK <rahul.nk@robosoftin.com>
2021-12-15 23:06:51 +0530

    remove test

--- src/components/test.js
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from 'react';
-import { cartContext } from '../context/cartContext';
-const Test = () => {
-	const cart = useContext(cartContext);
-	return <>{console.log(cart)}</>;
-};
-
-export default Test;
