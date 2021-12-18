f5f8d3308ff776c92ba449985a4099ce91b9672b
Rahul NK <rahul.nk@robosoftin.com>
2021-12-18 11:12:44 +0530

    commit

--- src/components/description/Items.js
+++ src/components/description/Items.js
@@ -1,63 +1,27 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import './Description.css';
-//import { itemData } from "./itemData";
-//  import {itemData} from './itemData';
-
-const Items = ({ itemm, price, amount, id, icon }) => {
-	const [count, setCount] = useState(1);
-
-	const incnum = () => {
-		setCount(count + 1);
-	};
-
-	const decnum = () => {
-		if (count > 1) {
-			setCount(count - 1);
-		} else {
-			setCount(1);
-		}
-	};
-
-	const [list, Setlist] = useState([]);
-	const handleRemove = (id) => {
-		const newitem = list.filter((item) => item.id !== id);
-		Setlist(newitem);
-
-		//  console.log("clicked")
-		// {itemData.filter(id)}
-	};
-
-	return (
-		<>
-			<div className="orderitems">
-				<div className="item">
-					<p className="itemname">{itemm} </p>
-					<div className="symbol">{icon}</div>
-=======
-import React, { useContext} from 'react';
+import React, { useContext } from 'react';
 import './Description.css';
 import { cartContext } from '../../context/cartContext';
-import vegicon from '../../assets/icons/vegicon.png'
-import nonveg from '../../assets/icons/nonveg.png'
+import vegicon from '../../assets/icons/vegicon.png';
+import nonveg from '../../assets/icons/nonveg.png';
 
-
-
-const Items=({ item,price,quantity,isVeg}) => {
-    
-const {deleteItem,increaseItemQuantity,decreaseItemQuantity}=useContext(cartContext)
-
-	
-	
+const Items = ({ item, price, quantity, isVeg }) => {
+	const { deleteItem, increaseItemQuantity, decreaseItemQuantity } =
+		useContext(cartContext);
 
 	return (
 		<>
-		
 			<div className="orderitems">
 				<div className="item">
 					<p className="itemname">{item} </p>
-					{isVeg?(<div><img src={vegicon} className="symbol" alt=""></img></div>):(<div><img src={nonveg} className="symbol" alt=""></img></div>)}
->>>>>>> a4a2ddd5506842111d5a61ab142a588c34a1b8a4
+					{isVeg ? (
+						<div>
+							<img src={vegicon} className="symbol" alt=""></img>
+						</div>
+					) : (
+						<div>
+							<img src={nonveg} className="symbol" alt=""></img>
+						</div>
+					)}
 				</div>
 				<div classname="cost">
 					<p className="icost">AED{price}</p>
@@ -68,29 +32,16 @@ const {deleteItem,increaseItemQuantity,decreaseItemQuantity}=useContext(cartCont
 					</p>
 				</div>
 				<div className="incdeccount">
-<<<<<<< HEAD
-					<div className="decbutton" onClick={decnum}>
-						_
-					</div>
-					<div className="number">{count}</div>
-					<div className="incbutton" onClick={incnum}>
-						+
-					</div>
-				</div>
-				<div className="itemtotalcost">AED{price * count}</div>
-				<div className="remove" onClick={() => handleRemove(id)}>
-=======
-					<div className="decbutton" onClick={()=>decreaseItemQuantity(item)}>
+					<div className="decbutton" onClick={() => decreaseItemQuantity(item)}>
 						_
 					</div>
 					<div className="number">{quantity}</div>
-					<div className="incbutton" onClick={()=>increaseItemQuantity(item)}>
+					<div className="incbutton" onClick={() => increaseItemQuantity(item)}>
 						+
 					</div>
 				</div>
 				<div className="itemtotalcost">AED{price * quantity}</div>
 				<div className="remove" onClick={() => deleteItem(item)}>
->>>>>>> a4a2ddd5506842111d5a61ab142a588c34a1b8a4
 					Remove
 				</div>
 				<div className="greyline"></div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import RestaurantData from '../components/restaurentsData';
-<<<<<<< HEAD
-=======
-
 
 import Payment from '../components/payment/Payment';
 
-import FinalPayment from '../components/finalpay/FinalPayment'
+import FinalPayment from '../components/finalpay/FinalPayment';
 
 import CartHome from '../components/carthome/CartHome';
 
->>>>>>> a4a2ddd5506842111d5a61ab142a588c34a1b8a4
 import Addresscomp from '../components/address/Address';
 import Home from '../containers/home';
 import Menu from '../containers/menu';
@@ -19,10 +15,7 @@ import OverView from '../components/menuComponent/overview';
 import Reviews from '../components/menuComponent/review';
 import Gallery from '../components/menuComponent/gallery';
 import CartContextProvider from '../context/cartContext';
-<<<<<<< HEAD
-=======
-import Cart from '../components/description/cart'
->>>>>>> a4a2ddd5506842111d5a61ab142a588c34a1b8a4
+import Cart from '../components/description/cart';
 const Routing = () => {
 	return (
 		<Router>
@@ -33,25 +26,20 @@ const Routing = () => {
 				<Route exact path="/restaurants/:searchKey/:location">
 					<RestaurantData />
 				</Route>
-<<<<<<< HEAD
-=======
 				<Route path="/cart">
-				<CartContextProvider>
-					<Cart/>
-				</CartContextProvider>
+					<CartContextProvider>
+						<Cart />
+					</CartContextProvider>
 				</Route>
 				<Route path="/address">
-				<Addresscomp/>
+					<Addresscomp />
 				</Route>
-				<Route  path="/payment" component={Payment}/>
-				
-				<Route path="/finalpay" component={FinalPayment}>
-					
-				</Route>
+				<Route path="/payment" component={Payment} />
+
+				<Route path="/finalpay" component={FinalPayment}></Route>
 				<Route path="/carthome">
-					<CartHome/>
-					</Route>
->>>>>>> a4a2ddd5506842111d5a61ab142a588c34a1b8a4
+					<CartHome />
+				</Route>
 				<Route path="/address">
 					<Addresscomp />
 				</Route>
