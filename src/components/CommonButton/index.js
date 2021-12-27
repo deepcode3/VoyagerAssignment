ffa798a37848ac7c8c30542c6071e6c2ce310659
Pavana S Salvankar <pavanasalvankarbelle1999@gmail.com>
2021-12-27 09:26:21 +0530

    make ignorecase false

---
+++ src/components/CommonButton/index.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const CommonButton = styled.button`
+  height: 50px;
+  width: 380px;
+  border-radius: 6px;
+  background: linear-gradient(138.33deg, #f3698e 0%, #feb456 100%);
+  box-shadow: 0 4px 10px 0 rgba(246, 126, 126, 0.38);
+  border: none;
+  display: flex;
+  justify-content: space-around;
+  color: #ffffff;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: -0.3px;
+  line-height: 22px;
+  text-align: center;
+  text-shadow: 0 0 9px 0 #ffffff;
+  padding-top: 15px;
+`;
+export default CommonButton;
---
+++ src/components/LoginComponents/LogoWithText/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LogoWithText = () => {
+  return (
+    <Wrapper>
+      <Lorem>LOREM</Lorem>
+      <Text>Experience the </Text>
+      <Text>easiest a way to get</Text>
+      <Line>
+        <BoldText>great food</BoldText>
+        <Text>Delivered</Text>
+      </Line>
+    </Wrapper>
+  );
+};
+
+export default LogoWithText;
+const Wrapper = styled.div`
+  background-image: url('../assets/images/login-background.png');
+  height: 588px;
+  width: 480px;
+  text-align: left;
+  padding-left: 4%;
+  margin-right: -4%;
+`;
+
+const Lorem = styled.p`
+  height: 60px;
+  width: 103px;
+  color: #ffffff;
+  font-family: 'Bebas Neue', sans-serif;
+  font-size: 30px;
+  margin-bottom: 10.5%;
+  margin-top: 35%;
+  transform: scaleY(1.4);
+  font-weight: 600;
+`;
+const Text = styled.p`
+  width: 323px;
+  color: #ffffff;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 32px;
+  letter-spacing: -0.57px;
+  text-shadow: 0 0 9px 0 #ffffff;
+  font-weight: 100;
+  margin-bottom: 1%;
+  margin-top: 0;
+`;
+const BoldText = styled.p`
+  width: 40%;
+  color: #ffffff;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 34px;
+  letter-spacing: -0.57px;
+  text-shadow: 0 0 9px 0 #ffffff;
+  font-weight: 600;
+  margin-bottom: 1%;
+  margin-top: 0;
+`;
+const Line = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
