cb1370bc93feecc030880851e7b852909d27d810
Pavana S Salvankar <pavanasalvankarbelle1999@gmail.com>
2021-12-27 12:33:11 +0530

    use localstorage to maintain login status

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,37 +1,38 @@
 import React from 'react';
 import SearchBar, {
-	DateTimeSetter,
-	LocationSetter,
+  DateTimeSetter,
+  LocationSetter,
 } from '../../components/homeComponents/searchbar';
 import mouse from '../../assets/icons/mouse.png';
 import './home.css';
 import Header from '../../components/Header';
-const Home = () => {
-	return (
-		<div className="homeBg">
-			<Header />
-			<div className="homeInfo">
-				<span className="homeInfo1">Order your food</span>
-				<br />
-				<span className="homeInfo2">from the best restaurants</span>
-			</div>
-			<div className="homeSearchBar">
-				<div className="homeSearch">
-					<SearchBar searchValue="" from="home" />
-				</div>
-				<div className="homeLocation">
-					<LocationSetter locationValue="" from="home" />
-				</div>
-				<div className="homeDate">
-					<DateTimeSetter />
-				</div>
-			</div>
 
-			<div className="scroller">
-				<img src={mouse} alt="mouse" className="ScrollImage"></img>
-				<div className="scrollInfo">Scroll</div>
-			</div>
-		</div>
-	);
+const Home = () => {
+  return (
+    <div className='homeBg'>
+      {localStorage.getItem('accessToken') ? <Header afterLogin /> : <Header />}
+      <div className='homeInfo'>
+        <span className='homeInfo1'>Order your food</span>
+        <br />
+        <span className='homeInfo2'>from the best restaurants</span>
+      </div>
+      <div className='homeSearchBar'>
+        <div className='homeSearch'>
+          <SearchBar searchValue='' from='home' />
+        </div>
+        <div className='homeLocation'>
+          <LocationSetter locationValue='' from='home' />
+        </div>
+        <div className='homeDate'>
+          <DateTimeSetter />
+        </div>
+      </div>
+
+      <div className='scroller'>
+        <img src={mouse} alt='mouse' className='ScrollImage' />
+        <div className='scrollInfo'>Scroll</div>
+      </div>
+    </div>
+  );
 };
 export default Home;
