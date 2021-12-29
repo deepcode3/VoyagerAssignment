fc9a63910ba16966ddc970056cb25628eae099ce
Pavana S Salvankar <pavanasalvankarbelle1999@gmail.com>
2021-12-29 13:02:37 +0530

    modified payment.js and .css

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -23,54 +23,56 @@ const Payment = () => {
   return (
     <>
       <CommonHeader />
-      <div className='paymentheading'>
-        <p className='paymenttext'>Payment</p>
-      </div>
-      <div className='paymentcontainer'>
-        <div className='Preferdpaymentdiv'>
-          <PaymentOption />
+      <div className='paymentbg'>
+        <div className='paymentheading'>
+          <p className='paymenttext'>Payment</p>
         </div>
-        <div className='carddetailscontainer'>
-          <p className='entercarddetailstext'>Enter Credit/Debit card details</p>
-          <div className='cardnumberdiv'>
-            <p className='cardnumbertext'>Card number</p>
-            <input type='text' className='cardnumber' />
-            <img src={visaicon} className='Visaicon' alt='' />
-            <div className='cardnumberline' />
+        <div className='paymentcontainer'>
+          <div className='Preferdpaymentdiv'>
+            <PaymentOption />
           </div>
-          <div className='nameoncarddiv'>
-            <p className='nameoncard'>Name on card</p>
-            <input type='text' className='namecard' />
-            <div className='namecardline' />
-          </div>
-          <div className='expirydatediv'>
-            <p className='expiry'>Expiry</p>
-            <p className='security'>Security card</p>
-            <input type='text' className='date' />
-            <div className='datelines' />
-            <div className='bar'>/</div>
-            <input type='text' className='year' />
-            <input type='text' className='code' />
+          <div className='carddetailscontainer'>
+            <p className='entercarddetailstext'>Enter Credit/Debit card details</p>
+            <div className='cardnumberdiv'>
+              <p className='cardnumbertext'>Card number</p>
+              <input type='text' className='cardnumber' />
+              <img src={visaicon} className='Visaicon' alt='' />
+              <div className='cardnumberline' />
+            </div>
+            <div className='nameoncarddiv'>
+              <p className='nameoncard'>Name on card</p>
+              <input type='text' className='namecard' />
+              <div className='namecardline' />
+            </div>
+            <div className='expirydatediv'>
+              <p className='expiry'>Expiry</p>
+              <p className='security'>Security card</p>
+              <input type='text' className='date' />
+              <div className='datelines' />
+              <div className='bar'>/</div>
+              <input type='text' className='year' />
+              <input type='text' className='code' />
 
-            <div className='yearline' />
-            <div className='codeline' />
+              <div className='yearline' />
+              <div className='codeline' />
+            </div>
+            <div className='sometext'>
+              <input type='radio' className='somename' />
+              <p className='addtosave'>Add this card to saved cards</p>
+            </div>
           </div>
-          <div className='sometext'>
-            <input type='radio' className='somename' />
-            <p className='addtosave'>Add this card to saved cards</p>
+
+          <PaymentPoints />
+          <p className='paymentdeliverydetails'>Delivery Details</p>
+          <PaymentDeliveryDetails />
+          <PaymentresDetails />
+
+          <div className='paybackbutton' onClick={loadaddress} role='button' onKeyDown={null}>
+            <img src={payback} alt='' />
           </div>
-        </div>
-
-        <PaymentPoints />
-        <p className='paymentdeliverydetails'>Delivery Details</p>
-        <PaymentDeliveryDetails />
-        <PaymentresDetails />
-
-        <div className='paybackbutton' onClick={loadaddress} role='button' onKeyDown={null}>
-          <img src={payback} alt='' />
-        </div>
-        <div className='paynowbutton' onClick={loadfinalpay} role='button' onKeyDown={null}>
-          <img src={paynow} alt='' />
+          <div className='paynowbutton' onClick={loadfinalpay} role='button' onKeyDown={null}>
+            <img src={paynow} alt='' />
+          </div>
         </div>
       </div>
     </>
