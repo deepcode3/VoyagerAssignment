e68231ffa3b091f622d8d96f5164c5ddd7e84934
Pavana S Salvankar <pavanasalvankarbelle1999@gmail.com>
2022-01-03 10:52:39 +0530

    move craetedAccounts to localstorage

--- src/context/AccountsContext/index.js
+++ src/context/AccountsContext/index.js
@@ -1,35 +1,44 @@
 import { React, useState, createContext } from 'react';
 import PropTypes from 'prop-types';
 
-const createdAccounts = [
-  {
-    firstname: 'John',
-    lastname: 'Salvankar',
-    email: 'john.salvankar@gmail.com',
-    password: '123',
-    conuntrycode: '91',
-    mobile: '97978787878',
-  },
-  {
-    firstname: 'Amar',
-    lastname: 'Anchan',
-    email: 'amar@gmail.com',
-    password: '123',
-    conuntrycode: '91',
-    mobile: '97978787878',
-  },
-];
-export const AccountsContext = createContext(createdAccounts);
+// const createdAccounts = [
+//   {
+//     firstname: 'John',
+//     lastname: 'Salvankar',
+//     email: 'john.salvankar@gmail.com',
+//     password: '123',
+//     conuntrycode: '91',
+//     mobile: '97978787878',
+//   },
+//   {
+//     firstname: 'Amar',
+//     lastname: 'Anchan',
+//     email: 'amar@gmail.com',
+//     password: '123',
+//     conuntrycode: '91',
+//     mobile: '97978787878',
+//   },
+// ];
+export const AccountsContext = createContext(JSON.parse(localStorage.getItem('createdAccounts')));
 const AccountsContextProvider = ({ children }) => {
-  const [accounts, setAccounts] = useState(createdAccounts);
+  const [accounts, setAccounts] = useState(JSON.parse(localStorage.getItem('createdAccounts')));
   const checkIfAccountExists = (data) => {
+    if (accounts == null) {
+      return null;
+    }
     const result = accounts.filter((obj) => {
       return obj.email === data.email;
     });
     return result;
   };
   const addAccount = (data) => {
-    setAccounts([...accounts, data]);
+    if (accounts === null) {
+      localStorage.setItem('createdAccounts', JSON.stringify(data));
+      setAccounts([data]);
+    } else {
+      setAccounts([...accounts, data]);
+      localStorage.setItem('createdAccounts', JSON.stringify([...accounts, data]));
+    }
   };
   return (
     /* eslint-disable react/jsx-no-constructed-context-values */
