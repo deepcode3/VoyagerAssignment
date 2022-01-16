4ecbc76dc5a048d356c186dbf40075377c53547c
Pavana S Salvankar <pavanasalvankarbelle1999@gmail.com>
2022-01-16 17:44:06 +0530

    create store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import store from './Store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
