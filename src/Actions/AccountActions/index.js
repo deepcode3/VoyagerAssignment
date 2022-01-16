dc3f255006f8cf4108a4bdafdff3c742abad9f44
Pavana S Salvankar <pavanasalvankarbelle1999@gmail.com>
2022-01-16 19:28:28 +0530

    change password

--- src/Actions/AccountActions/index.js
+++ src/Actions/AccountActions/index.js
@@ -4,3 +4,6 @@ export const addAccount = (account) => {
 export const editAccount = (account) => {
   return { type: 'EDIT_ACCOUNT', account };
 };
+export const changePassword = (data) => {
+  return { type: 'CHANGE_PASSWORD', data };
+};
--- src/Reducers/RootReducer/index.js
+++ src/Reducers/RootReducer/index.js
@@ -1,23 +1,33 @@
 /* eslint-disable  default-param-last */
+
 const initState = {
   currentUser: null,
   accounts: [],
 };
+
 const rootReducer = (state = initState, action) => {
   if (action === undefined) return state;
-  console.log('state', state);
   switch (action.type) {
-    case 'ADD_ACCOUNT': {
-      const newAccounts = [...state.accounts, action.account];
-      console.log('state', state);
-      return { ...state, accounts: newAccounts };
-    }
     case 'SIGN_OUT': {
       return { ...state, currentUser: null };
     }
     case 'SIGN_IN': {
-      console.log(action.data);
       return { ...state, currentUser: action.data };
+    }
+    case 'ADD_ACCOUNT': {
+      const newAccounts = [...state.accounts, action.account];
+      return { ...state, accounts: newAccounts };
+    }
+    case 'CHANGE_PASSWORD': {
+      const objIndex = state.accounts.findIndex((obj) => {
+        if (action.data.type === 'email') {
+          return obj.email === action.data.email;
+        }
+        return obj.countrycode === action.data.countrycode && obj.mobile === action.data.mobile;
+      });
+      const newAccounts = [...state.accounts];
+      newAccounts[objIndex].password = action.data.password;
+      return { ...state, accounts: newAccounts };
     }
     default:
       return state;
