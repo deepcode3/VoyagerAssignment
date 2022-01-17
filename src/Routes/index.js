959172e6eb0514b885adb8a627bf895fbda577e3
Pavana S Salvankar <pavanasalvankarbelle1999@gmail.com>
2022-01-17 19:02:08 +0530

    remove context providers from route

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -8,10 +8,9 @@ import FinalPayment from '../Components/Finalpay/FinalPayment';
 import CartHome from '../Components/CartHome/CartHome';
 import CartAddress from '../Components/Address/Address';
 import Home from '../Containers/Home';
-import CartContextProvider from '../Context/CartContext';
+
 import ReviewContext from '../Context/ReviewContext';
-import UserContextProvider from '../Context/UserContext';
-import AccountsContextProvider from '../Context/AccountsContext';
+
 import ProfileContextProvider from '../Context/ProfileContext';
 
 const Cart = lazy(() => {
@@ -56,89 +55,75 @@ const Gallery = lazy(() => {
 const Routing = () => {
   return (
     <Router>
-      <UserContextProvider>
-        <AccountsContextProvider>
-          <CartContextProvider>
-            <Switch>
-              <Route exact path='/'>
-                <Home />
-              </Route>
-              <Route exact path='/restaurants/:searchKey/:location'>
-                <RestaurantData />
-              </Route>
-              <Suspense
-                fallback={
-                  <div className='loader'>
-                    <CircularProgress color='secondary' />
-                  </div>
-                }
-              >
-                <Route path='/about' component={About} />
-                <Route path='/privacy-policy' component={PrivacyPolicy} />
-                <Route path='/terms-&-conditions' component={Terms} />
-                <Route path='/contact' component={Contacts} />
-                <Route path='/careers' component={Error} />
-                <Route path='/mobile-app' component={Error} />
-                <Route path='/blog' component={Error} />
-                <Route path='/sitemap' component={Error} />
-                <Route exact path='/cart'>
-                  <CartContextProvider>
-                    <CartLayout Component={Cart} />
-                  </CartContextProvider>
-                </Route>
-                <Route exact path='/address'>
-                  <ProfileContextProvider>
-                    <CartLayout Component={CartAddress} />
-                  </ProfileContextProvider>
-                </Route>
-                <Route path='/payment'>
-                  <CartLayout Component={Payment} />
-                </Route>
-                <Route path='/finalpay'>
-                  <ProfileContextProvider>
-                    <CartLayout Component={FinalPayment} />
-                  </ProfileContextProvider>
-                </Route>
-                <Route path='/cart-home'>
-                  <CartContextProvider>
-                    <CartLayout Component={CartHome} />
-                  </CartContextProvider>
-                </Route>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/restaurants/:searchKey/:location'>
+          <RestaurantData />
+        </Route>
+        <Suspense
+          fallback={
+            <div className='loader'>
+              <CircularProgress color='secondary' />
+            </div>
+          }
+        >
+          <Route path='/about' component={About} />
+          <Route path='/privacy-policy' component={PrivacyPolicy} />
+          <Route path='/terms-&-conditions' component={Terms} />
+          <Route path='/contact' component={Contacts} />
+          <Route path='/careers' component={Error} />
+          <Route path='/mobile-app' component={Error} />
+          <Route path='/blog' component={Error} />
+          <Route path='/sitemap' component={Error} />
+          <Route exact path='/cart'>
+            <CartLayout Component={Cart} />
+          </Route>
+          <Route exact path='/address'>
+            <ProfileContextProvider>
+              <CartLayout Component={CartAddress} />
+            </ProfileContextProvider>
+          </Route>
+          <Route path='/payment'>
+            <CartLayout Component={Payment} />
+          </Route>
+          <Route path='/finalpay'>
+            <ProfileContextProvider>
+              <CartLayout Component={FinalPayment} />
+            </ProfileContextProvider>
+          </Route>
+          <Route path='/cart-home'>
+            <CartLayout Component={CartHome} />
+          </Route>
 
-                <Route path='/status'>
-                  <CartContextProvider>
-                    <Orderstatus />
-                  </CartContextProvider>
-                </Route>
-                <Route path='/:searchKey/:location/menu/:restaurant/:item'>
-                  <CartContextProvider>
-                    <Menu />
-                  </CartContextProvider>
-                </Route>
-                <Route path='/:searchKey/:location/overview/:restaurant/:item'>
-                  <OverView />
-                </Route>
-                <Route path='/:searchKey/:location/reviews/:restaurant/:item'>
-                  <ReviewContext>
-                    <Reviews />
-                  </ReviewContext>
-                </Route>
-                <Route path='/:searchKey/:location/gallery/:restaurant/:item'>
-                  <Gallery />
-                </Route>
-                <Route path='/profile'>
-                  <ProfileContextProvider>
-                    <ProfileMain />
-                  </ProfileContextProvider>
-                </Route>
-                <Route path='/cart-layout'>
-                  <CartLayout />
-                </Route>
-              </Suspense>
-            </Switch>
-          </CartContextProvider>
-        </AccountsContextProvider>
-      </UserContextProvider>
+          <Route path='/status'>
+            <Orderstatus />
+          </Route>
+          <Route path='/:searchKey/:location/menu/:restaurant/:item'>
+            <Menu />
+          </Route>
+          <Route path='/:searchKey/:location/overview/:restaurant/:item'>
+            <OverView />
+          </Route>
+          <Route path='/:searchKey/:location/reviews/:restaurant/:item'>
+            <ReviewContext>
+              <Reviews />
+            </ReviewContext>
+          </Route>
+          <Route path='/:searchKey/:location/gallery/:restaurant/:item'>
+            <Gallery />
+          </Route>
+          <Route path='/profile'>
+            <ProfileContextProvider>
+              <ProfileMain />
+            </ProfileContextProvider>
+          </Route>
+          <Route path='/cart-layout'>
+            <CartLayout />
+          </Route>
+        </Suspense>
+      </Switch>
     </Router>
   );
 };
