6d1c4c15af9903f78a43dcc28b8d1e35243275df
Pavana S Salvankar <pavanasalvankarbelle1999@gmail.com>
2022-01-18 10:34:41 +0530

    show cart only if the user is logged in

--- src/Components/CartHome/CartHome.js
+++ src/Components/CartHome/CartHome.js
@@ -10,27 +10,37 @@ const CartHome = () => {
   const currentUser = useSelector((state) => {
     return state.currentUser;
   });
-  const resdata = data.filter((obj) => {
-    return restaurantItemsCount(obj.name, currentUser) !== 0;
-  });
+  let resdata = null;
+  if (currentUser !== null) {
+    resdata = data.filter((obj) => {
+      return restaurantItemsCount(obj.name, currentUser) !== 0;
+    });
+  }
+
   return (
     <div className='cartbg'>
-      <p className='mycarttext'>
-        My Cart
-        <span>{`(${resdata.length})`}</span>
-      </p>
-      {resdata.map((val) => {
-        // eslint-disable-next-line react/jsx-wrap-multilines
-        return (
-          <Cartlist
-            key={val.name}
-            Hotelname={val.name}
-            hoteladdress={val.location}
-            totalproduct={restaurantItemsCount(val.name, currentUser)}
-            productcost={totalPrice(val.name, currentUser)}
-          />
-        );
-      })}
+      {currentUser === null ? (
+        <p className='logintext'>Please login to check your cart</p>
+      ) : (
+        <>
+          <p className='mycarttext'>
+            My Cart
+            <span>{`(${resdata.length})`}</span>
+          </p>
+          {resdata.map((val) => {
+            // eslint-disable-next-line react/jsx-wrap-multilines
+            return (
+              <Cartlist
+                key={val.name}
+                Hotelname={val.name}
+                hoteladdress={val.location}
+                totalproduct={restaurantItemsCount(val.name, currentUser)}
+                productcost={totalPrice(val.name, currentUser)}
+              />
+            );
+          })}
+        </>
+      )}
     </div>
   );
 };
