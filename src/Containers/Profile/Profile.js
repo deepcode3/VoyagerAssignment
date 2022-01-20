1acbc14ed4ba34db3714a22f4f5c160fdb4ced4c
Pavana S Salvankar <pavanasalvankarbelle1999@gmail.com>
2022-01-20 14:20:47 +0530

    ignore indent error in profile page

--- src/Containers/Profile/Profile.js
+++ src/Containers/Profile/Profile.js
@@ -1,4 +1,5 @@
 /* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable indent */
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
